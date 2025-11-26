fb617f5ec444baa81f9fccce55aebe14fda9115e
Archive 93 <archive.93.contact@gmail.com>
2025-11-26 16:26:16 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -57,10 +57,10 @@ export default function Home() {
                                     <ChevronRight className="w-5 h-5 ml-2" />
                                 </Button>
                             </Link>
-                            <Link to={createPageUrl('AddNation')}>
+                            <Link to={createPageUrl('ContinentalCompetitions')}>
                                 <Button size="lg" variant="outline" className="text-lg px-8 border-white/30 text-white hover:bg-white/10">
-                                    <Plus className="w-5 h-5 mr-2" />
-                                    Add Nation
+                                    <Star className="w-5 h-5 mr-2" />
+                                    Continental Cups
                                 </Button>
                             </Link>
                         </div>
