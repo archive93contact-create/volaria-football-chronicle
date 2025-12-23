a1106120db1b16c23e405ec7303710ffcea803a8
Archive 93 <archive.93.contact@gmail.com>
2025-12-23 17:22:38 +0000

    File changes

--- src/pages/NationDetail.jsx
+++ src/pages/NationDetail.jsx
@@ -284,7 +284,7 @@ export default function NationDetail() {
 
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
                 {/* Nation Stats */}
-                <NationStats nation={nation} clubs={clubs} leagues={leagues} coefficient={coefficient} />
+                <NationStats nation={nation} clubs={clubs} leagues={leagues} coefficient={coefficient} nationalTeamStrength={nationalTeamStrength} />
 
                 {/* Quick Access Cards */}
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-8">
